b1b8b7a3fcc4aad5c68a9a3652a3e76a058ac81e
Hussein Hussein <husseinwkc@gmail.com>
2023-03-14 16:23:24 -0400

    added exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,10 @@
 import Hadith from "./hadith.model";
 import HadithCategories from "./hadithCategories.model";
 import HadithLanguage from "./hadithLanguage.model";
-
+import HijriDate from "./date.model";
 export {
     Hadith,
     HadithCategories,
     HadithLanguage,
+    HijriDate
 }
